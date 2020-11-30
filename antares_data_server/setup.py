41467a7dcd64966b710afe22638958a975748cb4
andreatramacere <andrea.tramacere@gmail.com>
2020-11-30 14:37:21 +0100

    refactoring repo, removing antares docker

--- antares_data_server/setup.py
+++ antares_data_server/setup.py
@@ -34,6 +34,23 @@ __version__ = _info['version']
 include_package_data=True
 
 scripts_list=glob.glob('./bin/*')
+
+
+def compile_and_install_software():
+    """Used the subprocess module to compile/install the C software."""
+    src_path = './some_c_package/'
+
+    # compile the software
+    cmd = "./configure CFLAGS='-03 -w -fPIC'"
+    venv = get_virtualenv_path()
+    if venv:
+        cmd += ' --prefix=' + os.path.abspath(venv)
+    subprocess.check_call(cmd, cwd=src_path, shell=True)
+
+    # install the software (into the virtualenv bin dir if present)
+    subprocess.check_call('make install', cwd=src_path, shell=True)
+
+
 setup(name='antares_data_server',
       version=__version__,
       description='A Python Framework for MAGIC high-level data distribution',
--- demoMM-master/make.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 multiMessenger.cc -o multiMessenger `root-config --cflags --glibs`
